fb4aa5734a021a0ae9f18197b2faa9c4c120d73c
Roman <spiderr.roma@gmail.com>
2023-03-24 11:38:04 +0200

    wallpaper added

--- hypr/randwall.sh
+++ hypr/randwall.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-swww img -t any --transition-bezier 0.0,0.0,1.0,1.0 --transition-duration .75 --transition-step 255 --transition-fps 60 /home/bladen/Wallpapers/"$(ls /home/bladen/Wallpapers/ | shuf -n 1)"
+swww img -t any --transition-bezier 0.0,0.0,1.0,1.0 --transition-duration .75 --transition-step 255 --transition-fps 60 ~/.config/wallpapers/"$(ls ~/.config/wallpapers/ | shuf -n 1)"
